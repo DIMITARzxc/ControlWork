2fc33a2b5b1606fd1f301ccf4cf933f8df30109f
DIMITARzxc <54147589+DIMITARzxc@users.noreply.github.com>
2019-08-20 23:28:37 +0300

    Update Program.cs

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,64 +8,54 @@ namespace ControlWork
 {
     class Program
     {
-        struct City
-        {
-            public int Population;
-            public int Name;
-            public int Denisty;
-
-        }
         static void Main(string[] args)
         {
-            string s;
-            
-            Console.WriteLine("Enter city name, population,square ");
-            s = Console.ReadLine();
-            string[] words = s.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-           // string[] popul = s.Split(',');
-            
-           /* for(int i=0; i<popul.Length; i++)
+            //string s= "Kharkiv=143000,320;Kqwrtyqw=12490,319;LosAngeles=124000,450" ;
+            Console.WriteLine("Input:");
+             string s = Console.ReadLine();
+            string[] input = s.Split(';');
+            string[] city = new string[input.Length];
+            string[] elemnt = new string[input.Length];
+            string[] square = new string[elemnt.Length];
+            for(int i=0; i < city.Length-1; i++)
             {
+                city = input[i].Split('=');
                
-                Console.WriteLine(int.Parse("popul"));
-            }*/
-           // Console.WriteLine(int.Parse("popul"));
-            
-            string[] town = new string[words.Length];
-            for(int i=0; i<words.Length; i++)
-            {
-                 town = words[i].Split('=');
-                if (town[i].Length > town[i + 1].Length)
-                {
 
-                    Console.WriteLine("Most populated town =" + town[i]);
-                }
-                else
-                {
-                    Console.WriteLine("Most populated town = " + town[i + 1]);
-                }
-                
-                
-                Console.WriteLine(town[0]);
-                
             }
-            string[] numbers = new string[town.Length];
-            string[] kil = new string[numbers.Length];
-            for (int i = 0; i < words.Length; i++)
+            if (city[0].Length > city[1].Length && city[0].Length>city[2].Length)
             {
-                numbers = words[i].Split(',');
+                Console.WriteLine("Longest name " + city[0], city[0].Length);
+            }
+            if (city[0].Length < city[1].Length || city[1].Length > city[2].Length)
+            {
+                Console.WriteLine("Longest name " + city[1], city[1].Length);
+            }
+            if (city[2].Length > city[0].Length || city[2].Length > city[1].Length)
+            {
+                Console.WriteLine("Longest name " + city[2], city[2].Length);
+            }
+            for (int i = 0; i < square.Length - 1; i++)
+            {
+                square = input[i].Split(',');
+                
+                
+
+            }
+            //Console.WriteLine(square[0]);
+            string[] numbers = new string[input.Length];
+            string[] kil = new string[numbers.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers = input[i].Split(',');
                 kil = numbers[i].Split(',');
                 Console.WriteLine(numbers[0]);
                 Console.WriteLine(kil[0]);
 
             }
-            
 
-           
-           
-            Console.Read();
-
-
+            //           Console.WriteLine(input[0]);
+            Console.ReadKey();
         }
     }
 }
